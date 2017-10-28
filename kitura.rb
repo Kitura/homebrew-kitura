6c7768e0a40c26202385bd7317618480e0df8674
Ian Partridge <i.partridge@uk.ibm.com>
2017-10-28 21:18:23 +0100

    Upgrade kitura to 0.0.6

--- kitura.rb
+++ kitura.rb
@@ -3,13 +3,12 @@ require "language/node"
 class Kitura < Formula
   desc "Kitura command-line interface"
   homepage "https://github.com/IBM-Swift/kitura-cli#readme"
-  url "https://registry.npmjs.org/kitura-cli/-/kitura-cli-0.0.5.tgz"
-  version "0.0.5"
-  sha256 "e6ac1b843689d625addaddb5bc4863566c7ff21868aa529dbcc1591f8af02cb7"
+  url "https://registry.npmjs.org/kitura-cli/-/kitura-cli-0.0.6.tgz"
+  version "0.0.6"
+  sha256 "279e5b5b29b22ec357ee36dd58a7fd192f6b5a7a6c95383a342406d490b94db9"
 
   depends_on "node"
   depends_on "yo"
-  depends_on "generator-swiftserver"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
