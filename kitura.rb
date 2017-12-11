c98693fc320126a83717c260cc02a689e9599a95
Ian Partridge <i.partridge@uk.ibm.com>
2017-12-11 12:23:51 +0000

    Upgrade kitura to 0.0.9

--- kitura.rb
+++ kitura.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Kitura < Formula
   desc "Kitura command-line interface"
   homepage "https://github.com/IBM-Swift/kitura-cli#readme"
-  url "https://registry.npmjs.org/kitura-cli/-/kitura-cli-0.0.8.tgz"
-  version "0.0.8"
-  sha256 "807334a6a812efbde21478428a5924d7168533660ff69574638a8f52fb27117f"
+  url "https://registry.npmjs.org/kitura-cli/-/kitura-cli-0.0.9.tgz"
+  version "0.0.9"
+  sha256 "87cbbf08dd764ffb89753a396f8151be7d79c6ad0fe8b13fb85d3e5c592e5778"
 
   depends_on "node"
 
