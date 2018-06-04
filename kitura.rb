c6b3ff59a8361cd6fc4eb86390ad46a827888b06
Alex Wishart <awishart@uk.ibm.com>
2018-06-04 15:42:30 +0100

    Update version to 0.0.11

--- kitura.rb
+++ kitura.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Kitura < Formula
   desc "Kitura command-line interface"
   homepage "https://github.com/IBM-Swift/kitura-cli#readme"
-  url "https://registry.npmjs.org/kitura-cli/-/kitura-cli-0.0.9.tgz"
-  version "0.0.9"
-  sha256 "87cbbf08dd764ffb89753a396f8151be7d79c6ad0fe8b13fb85d3e5c592e5778"
+  url "https://registry.npmjs.org/kitura-cli/-/kitura-cli-0.0.11.tgz"
+  version "0.0.11"
+  sha256 "e1da20ceb187930028204532b764957a4baa7dc29b74d0f7dd200a931ce79764"
 
   depends_on "node"
 
