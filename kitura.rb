f68eab40977999031954a03fe4b30724b39f124b
Danny Sung <danny@dannysung.com>
2021-03-05 21:59:14 -0800

    Add 0.2.0 release

--- kitura.rb
+++ kitura.rb
@@ -1,9 +1,9 @@
 class Kitura < Formula
   desc "Kitura command-line interface"
   homepage "https://github.com/Kitura/kitura-cli#readme"
-  url "https://github.com/Kitura/kitura-cli/releases/download/0.1.0/kitura-cli_0.1.0_darwin.tar.gz"
-  version "0.1.0"
-  sha256 "bf8c529cfbfb2fd8a02ba95ff9a1f557d98373ac573f632889a084f0926be844"
+  url "https://github.com/Kitura/kitura-cli/releases/download/0.2.0/kitura-cli_0.2.0_darwin.tar.gz"
+  version "0.2.0"
+  sha256 "752362237ff6fbbc3198b5a64f64cb94f26fe5e8514a0537dafd1d3684cfeb95"
 
   def install
     bin.install "kitura"
